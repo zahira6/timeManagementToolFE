b3ed7c29f9c539eeaa32cc2946e1dccc3d3d5446
zahira6 <steffihermanns@gmx.de>
2023-01-27 12:37:18 +0100

    added rest of design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from './user/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'timeManagementToolFE';
+
+  constructor(private userService: UserService){}
+
+  userIsLoggedIn(): boolean {
+    if(this.userService.isLoggedIn()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
